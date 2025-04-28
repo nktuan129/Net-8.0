ea2840aea9fee9aa04584cb22aa7a93968ec5f3f
TuanNK <nktuan1209@gmail.com>
2025-04-28 11:23:28 +0700

    create project

---
+++ customers_management/customers_management/bin/Debug/net8.0-windows/Program.vb
@@ -0,0 +1,8 @@
+﻿Module Program
+    <STAThread>
+    Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Signin())
+    End Sub
+End Module
